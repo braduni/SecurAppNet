ad0d11130b2decb3f66e6754f7c63e50335820fa
Brad <bradley1@protonmail.com>
2023-08-15 16:59:09 +0100

    Add dbcontext

---
+++ SecurAppNet/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SecurAppNet.Models;
+
+namespace SecurAppNet.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) 
+            : base(options) 
+        { 
+        }
+
+        public DbSet<User> Users => Set<User>();  
+    }
+}
